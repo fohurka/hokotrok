770ed9c163bd2d2abba276b6cc4060c349911fa7
huse-mate <husemate01@gmail.com>
2026-03-26 17:49:39 +0100

    refined the Test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,42 +1,92 @@
 public class Test {
     public abstract class TestCase {
+        public abstract String getName();
+
+        public abstract void setup();
+
         public abstract void execute();
     }
 
     public class JunctionProgressCar extends TestCase {
+        private Junction loc;
+        private Car c;
+
+        @Override
+        public String getName() {
+            return "JunctionProgressCar";
+        }
+
+        @Override
+        public void setup() {
+            loc = new Junction();
+            c = new Car(loc);
+        }
+
         @Override
         public void execute() {
-            Junction loc = new Junction();
-            Car c = new Car(loc);
             c.tick();
         }
     }
 
     public class JunctionProgressBus extends TestCase {
+        private Junction loc;
+        private Bus b;
+
+        @Override
+        public String getName() {
+            return "JunctionProgressBus";
+        }
+
+        @Override
+        public void setup() {
+            loc = new Junction();
+            b = new Bus(loc);
+        }
+
         @Override
         public void execute() {
-            Junction loc = new Junction();
-            Bus b = new Bus(loc);
             b.tick();
         }
     }
 
     public class JunctionProgressSnowplow extends TestCase {
+        private Junction loc;
+        private Snowplow sn;
+
+        @Override
+        public String getName() {
+            return "JunctionProgressSnowplow";
+        }
+
+        @Override
+        public void setup() {
+            loc = new Junction();
+            sn = new Snowplow(loc);
+        }
+
         @Override
         public void execute() {
-            Junction loc = new Junction();
-            Snowplow sn = new Snowplow(loc);
             sn.tick();
         }
     }
 
     public class JunctionChoiceCar extends TestCase {
+        private CarPlayer p;
+        private Lane dest;
+
+        @Override
+        public String getName() {
+            return "JunctionChoiceCar";
+        }
+
+        @Override
+        public void setup() {
+            // TODO
+        }
+
         @Override
         public void execute() {
-            Junction loc = new Junction();
-
-            Car c = new Car(loc);
-            // TODO
+            p.choseDirection(dest, 0);
         }
     }
 }
