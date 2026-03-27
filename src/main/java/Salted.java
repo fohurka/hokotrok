eed3783b4e1e16e1970e5d0df2f817e347684c3c
Balint <vajdabalint2004@gmail.com>
2026-03-27 16:30:01 +0100

    small fix to Salted.applyWeather function

--- src/main/java/Salted.java
+++ src/main/java/Salted.java
@@ -3,6 +3,7 @@ public class Salted extends Modifier {
     @Override
     public void applyWeather(Surface surf) {
         Skeleton.printFunctionCall("Salted.applyWeather");
+        surf.removeSnow(Skeleton.askInt("Salted.applyWeather: amount of snow to remove"));
         surf.removeIce(Skeleton.askInt("Salted.applyWeather: amount of ice to remove"));
         Skeleton.printReturn();
     }
